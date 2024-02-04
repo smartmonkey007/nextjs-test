7adbdc60da5f06398946423f19c40e3fbcb72a10
Marc Meugniot <Marc@think007.com>
2024-02-04 15:48:28 -0800

    Update AcmeLogo component styling

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -4,7 +4,7 @@ import { lusitana } from '@/app/ui/fonts';
 export default function AcmeLogo() {
   return (
     <div
-      className={`flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <MapPinIcon className="h-12 w-12 rotate-[-35deg] pr-3" />
       <p className="text-[44px]">LifeQuest</p>
