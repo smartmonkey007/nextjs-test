326264d26ae9a488650354e46387bf687b0625b3
Marc Meugniot <Marc@think007.com>
2024-02-04 15:47:07 -0800

    Fix import statement in AcmeLogo component

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,5 +1,5 @@
 import { MapPinIcon } from '@heroicons/react/24/outline';
-// import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
